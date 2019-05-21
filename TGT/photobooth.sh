41be3d070ac15167054d574c4adfdd5c7fd837a3
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2019-05-20 21:10:40 -0400

    Add PhotoBooth defaults

--- TGT/photobooth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "Photo Booth"
-echo "###############################################################################"
-
-echo ""
-echo "Enable the debug menu in Photo Booth"
-defaults write com.apple.PhotoBooth EnableDebugMenu 1
