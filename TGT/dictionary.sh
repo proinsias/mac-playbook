66120b78cb69c03854b48cca8b8d52de03074b05
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2019-05-20 21:04:03 -0400

    Add Dictionary defaults

--- TGT/dictionary.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "Dictionary"
-echo "###############################################################################"
-
-#Reuse dictionary definition window:
-#$ defaults write com.apple.Dictionary ProhibitNewWindowForRequest -boolean
