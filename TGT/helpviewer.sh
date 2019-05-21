817d8c7144abc2e0315cf75634ca425b8d7b6508
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2019-05-20 21:05:11 -0400

    Add Help Viewer defaults

--- TGT/helpviewer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "Help Viewer"
-echo "###############################################################################"
-
-echo ""
-echo "Set Help Viewer windows to non-floating mode"
-defaults write com.apple.helpviewer DevMode -bool true
