5faa273c0d3998f9e8a50677d21f6b9df4459c17
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2019-05-20 21:20:00 -0400

    Add GPGMail and MenuMeters defaults

--- TGT/gpgmail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "GPGMail 2"
-echo "###############################################################################"
-
-#echo ""
-#echo "Disable signing emails by default"
-#defaults write ~/Library/Preferences/org.gpgtools.gpgmail SignNewEmailsByDefault -bool false
--- TGT/menumeters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "Menu Meters"
-echo "###############################################################################"
-
-echo ""
-echo "Update disk information only every 1s)"
-defaults write com.ragingmenace.MenuMeters.plist DiskInterval 1.0
