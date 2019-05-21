f5fdc98bbb198b4029909f4c5020b45c821c0a12
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2019-05-20 21:07:58 -0400

    Add Reminders defaults

--- TGT/reminders.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "Reminders.app"
-echo "###############################################################################"
-
-echo ""
-echo "Show debug menu"
-defaults write com.apple.reminders RemindersDebugMenu -boolean true
