d6487334dd41805c1a68c4bace7f52cbae1b2b93
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2019-05-20 21:09:39 -0400

    Add System Preferences/Users & Groups defaults

--- TGT/systemprefs_users.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "System Preferences/Users & Groups"
-echo "###############################################################################"
-
-echo ""
-echo "Show fast user switching menu as [Icon]"
-defaults write -g userMenuExtraStyle -int 2
