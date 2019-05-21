1b7cffbd79f894b7c49a8b6712e8484b724b536c
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2019-05-20 21:07:12 -0400

    Add QuickTime Player defaults

--- TGT/quicktimeplayer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "###############################################################################"
-echo "QuickTime Player"
-echo "###############################################################################"
-
-echo ""
-echo "Auto-play videos when opened with QuickTime Player"
-defaults write com.apple.QuickTimePlayerX MGPlayMovieOnOpen -bool true
