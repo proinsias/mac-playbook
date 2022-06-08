f8099267d63d7858439a792c11d332b7bf019489
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2022-06-08 09:58:17 -0400

    Address globstar issue

--- bin/install_ci_dependencies.sh
+++ bin/install_ci_dependencies.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 set -o xtrace                 # Copy command to output.
+set -o errexit                # Exit on error. Append || true if you expect an error.
+set -o errtrace               # Exit on error inside any functions or subshells.
+set -o noclobber              # Don't allow overwriting files.
+set -o nounset                # Don't allow use of undefined vars. Use ${VAR:-} to use an undefined VAR.
+set -o pipefail               # Produce a failure return code if any pipeline command errors.
+shopt -s failglob             # Cause globs that don't get expanded to cause errors.
 # FIXME:
-#set -o errexit                # Exit on error. Append || true if you expect an error.
-#set -o errtrace               # Exit on error inside any functions or subshells.
-#set -o noclobber              # Don't allow overwriting files.
-#set -o nounset                # Don't allow use of undefined vars. Use ${VAR:-} to use an undefined VAR.
-#set -o pipefail               # Produce a failure return code if any pipeline command errors.
-#shopt -s failglob             # Cause globs that don't get expanded to cause errors.
 #shopt -s globstar 2>/dev/null # Match all files and zero or more sub-directories.
 
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install ansible-core ansible-lint yamllint
+sudo --set-home python3 -m pip install --upgrade pip
+sudo --set-home python3 -m pip install ansible-core ansible-lint yamllint
 ./bin/install_dependencies.sh
